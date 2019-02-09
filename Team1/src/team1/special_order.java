e773f89df44d2fcb2b83bd071a7c6fb852a1a3a4
1894343 <1894343@E210R12.CEGEPGIM.LCL>
2019-02-09 11:00:17 -0500

    commiting special order file

---
+++ Team1/src/team1/special_order.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1894343
+ */
+public class special_order extends Order {
+
+    public special_order(Date date, String number) {
+        super(date, number);
+    }
+    
+  public void dispatch()
+  {
+      
+  }
+    
+
+   
+  
+}
