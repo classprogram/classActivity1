a74a412b741c8c7b5c3f79a279ec4aa3d6fff036
Sharad <.gitsharadphw069@gmail.com>
2019-02-09 11:01:42 -0500

    Merge origin/master
    Conflicts:
    	src/java_app/customer.java

---
+++ Team1/src/team1/customer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team1;
+
+/**
+ *
+ * @author temp
+ */
+public class customer {
+    String custname;
+    String custlocation;
+    
+      public customer(String thename, String thelocation)
+   {
+       this.custname= thename;
+       this.custlocation= thelocation;
+       
+   }
+   
+    public void sendOrder()
+    {
+        System.out.println("send order");
+    }
+    public void receiveOrder()
+    {
+        System.out.println("receive order");
+    }
+}
