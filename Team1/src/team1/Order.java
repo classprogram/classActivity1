9fbb64a012f50f9011eb83bd6352e0449f4319fc
1895730 <1895730@E21033.CEGEPGIM.LCL>
2019-02-09 10:52:46 -0500

    Commiting order class by pavani

---
+++ Team1/src/team1/Order.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1895730
+ */
+public class Order {
+    Date date;
+    String number;
+    
+    
+   public Order(Date date, String number)
+   {
+       this.date= date;
+       this.number= number;
+       
+   }
+   
+   public void confirm()
+   {
+       
+   }
+   
+   
+    public void close()
+    {
+        
+    }
+    
+}
